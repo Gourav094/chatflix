1722ba937d5b2583a7b251c024cdd4b533b9da21
gourav garg <garggourav012@gmail.com>
2024-01-27 14:52:16 +0530

    Fix tv show for search cards

--- src/components/GPTMovieSuggestion.js
+++ src/components/GPTMovieSuggestion.js
@@ -3,19 +3,29 @@ import { useSelector } from 'react-redux'
 import MoviesList from './MoviesList'
 import MovieCard from './MovieCard'
 import { Link } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { addClickedMovie } from '../utils/movieInfoSlice'
+
 const GPTMovieSuggestion = () => {
+    const dispatch = useDispatch()
     const movies = useSelector(store => store.search.searchMovies)
     const query = useSelector(store => store.search.searchQuery)
-    if(!movies)return
+    const handleMovieClick = (movie) => {
+        dispatch(addClickedMovie(movie))
+    }
+    if (!movies) return
     return (
         <div className='ml-20 md:mx-36 min-h-screen'>
             {query && <div className='text-white text-lg py-2 md:pl-[18%]'>search result for: <span className='pl-2 font-semibold'>{query}</span></div>}
 
             {query && movies.length === 0 && (
-                <h1 className='md:pl-[18%] text-yellow-600 font-semibold text-lg'>Result not found! Please check spelling</h1> )}
-            
+                <h1 className='md:pl-[18%] text-yellow-600 font-semibold text-lg'>Result not found! Please check spelling</h1>)}
+
             <div className='h-full flex flex-wrap gap-6 pt-4'>
-                {movies.map((movie) =>  <Link key ={movie.id} to={"/info/"+ movie?.id}><MovieCard poster_path={movie.poster_path} title={movie.title || movie.name} rating={movie.vote_average} /></Link>
+                {movies.map((movie) => <Link key={movie.id} to={"/info/" + movie?.id}>
+                    <div onClick={() => handleMovieClick(movie)}>
+                        <MovieCard poster_path={movie.poster_path} title={movie.title || movie.name} rating={movie.vote_average} />
+                        </div></Link>
                 )}
             </div>
 
